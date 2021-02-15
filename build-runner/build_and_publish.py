3771afc7bc166cb9f6f78d36c008c62643ee59a7
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2021-02-15 11:34:30 +0900

    checking in

--- build-runner/build_and_publish.py
+++ build-runner/build_and_publish.py
@@ -11,6 +11,7 @@ parser = ArgumentParser(description='Build and publish qc-workbook.')
 parser.add_argument('--account', '-a', metavar='ACCOUNT', dest='account', default='UTokyo-ICEPP', help='Github account of the source repository.')
 parser.add_argument('--branch', '-b', metavar='BRANCH', dest='branch', default='master', help='Branch from which to build the website.')
 parser.add_argument('--no-checkout', '-n', action='store_true', dest='no_checkout', help='Don\'t checkout from github.')
+parser.add_argument('--ignore-errors', '-g', action='store_true', dest='ignore_errors', help='Run build with --keep-going flag.')
 parser.add_argument('--work-dir', '-w', metavar='PATH', dest='workdir', default='/tmp', help='Working area.')
 options = parser.parse_args()
 sys.argv = []
@@ -23,4 +24,16 @@ if not options.no_checkout:
 
 os.chdir('{}/qc-workbook'.format(options.workdir))
 
-build.callback('source/jp', '/build', None, None, False, False, False, False, 'html', None, 0, 0, False)
+build.callback(path_source='source/jp',
+               path_output='/build',
+               config=None,
+               toc=None,
+               warningiserror=False,
+               nitpick=False,
+               keep_going=options.ignore_errors,
+               freshenv=False,
+               builder='html',
+               custom_builder=None,
+               verbose=0,
+               quiet=0,
+               individualpages=False)
