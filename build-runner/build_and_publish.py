7199be562f3221075f269bb05916217fdcadcff9
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2021-02-15 14:13:36 +0900

    making checkout store_true

--- build-runner/build_and_publish.py
+++ build-runner/build_and_publish.py
@@ -8,29 +8,31 @@ from argparse import ArgumentParser
 from jupyter_book.commands import build
 
 parser = ArgumentParser(description='Build and publish qc-workbook.')
+parser.add_argument('--checkout', '-k', action='store_true', dest='checkout', help='Checkout the source files from github.')
 parser.add_argument('--account', '-a', metavar='ACCOUNT', dest='account', default='UTokyo-ICEPP', help='Github account of the source repository.')
 parser.add_argument('--branch', '-b', metavar='BRANCH', dest='branch', default='master', help='Branch from which to build the website.')
-parser.add_argument('--no-checkout', '-n', action='store_true', dest='no_checkout', help='Don\'t checkout from github.')
-parser.add_argument('--ignore-errors', '-g', action='store_true', dest='ignore_errors', help='Run build with --keep-going flag.')
-parser.add_argument('--work-dir', '-w', metavar='PATH', dest='workdir', default='/tmp', help='Working area.')
+parser.add_argument('--source', '-i', metavar='PATH', dest='source', default='/tmp/qc-workbook/source', help='Source directory.')
+parser.add_argument('--target', '-o', metavar='PATH', dest='target', default='/build', help='Build directory.')
+parser.add_argument('--publish', )
 options = parser.parse_args()
 sys.argv = []
 
-if not options.no_checkout:
-    shutil.rmtree('{}/qc-workbook'.format(options.workdir), ignore_errors=True)
-    os.chdir(options.workdir)
+if options.checkout:
+    workdir = os.path.dirname(options.source)
+    shutil.rmtree(workdir, ignore_errors=True)
+    os.chdir(os.path.dirname(workdir))
     # Can think about installing gitpython if needed
     subprocess.Popen(['git', 'clone', '-b', options.branch, 'https://github.com/{}/qc-workbook'.format(options.account)]).wait()
 
-os.chdir('{}/qc-workbook'.format(options.workdir))
+os.chdir(options.source)
 
-build.callback(path_source='source/jp',
-               path_output='/build',
+build.callback(path_source='jp',
+               path_output=options.target,
                config=None,
                toc=None,
                warningiserror=False,
                nitpick=False,
-               keep_going=options.ignore_errors,
+               keep_going=False,
                freshenv=False,
                builder='html',
                custom_builder=None,
