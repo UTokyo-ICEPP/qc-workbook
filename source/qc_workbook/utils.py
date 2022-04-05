70072c934cb9361e4c8f6cda8b51098fb5549b62
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2022-04-05 16:41:41 +0900

    assignment week 2 updated

--- source/qc_workbook/utils.py
+++ source/qc_workbook/utils.py
@@ -1,21 +1,46 @@
 """Utility functions for qc-workbook."""
 
+from typing import Callable, Optional
 import collections
 import numpy as np
 from qiskit.providers.backend import BackendV1 as Backend
 
-def operational_backend(min_qubits: int = 0):
+def operational_backend(
+    min_qubits: int = 0,
+    min_qv: int = 0,
+    qubits: Optional[int] = None,
+    qv: Optional[int] = None
+) -> Callable:
+    
     def backend_filter(backend):
         if not backend.status().operational:
             return False
         
         config = backend.configuration()
-        return (not config.simulator) and config.n_qubits >= min_qubits
+        
+        if config.simulator:
+            return False
+        
+        if qubits is not None:
+            if config.n_qubits != qubits:
+                return False
+        else:
+            if config.n_qubits < min_qubits:
+                return False
+
+        if qv is not None:
+            if config.quantum_volume != qv:
+                return False
+        else:
+            if config.quantum_volume < min_qv:
+                return False
+        
+        return True
 
     return backend_filter
 
 
-def find_best_chain(backend: Backend, length: int):
+def find_best_chain(backend: Backend, length: int, return_error_prod: bool = False):
     """Find a chain of qubits with the smallest product of CNOT and measurement errors.
     """
     
@@ -48,14 +73,19 @@ def find_best_chain(backend: Backend, length: int):
     # Find the chain with the smallest error (CX and readout) product
     prop = backend.properties()
 
-    min_log_error = 0.
+    min_log_gate_error = 0.
+    min_log_readout_error = 0.
     best_chain = None
     for chain in chains:
-        log_error = sum(np.log(prop.gate_error('cx', [q1, q2])) for q1, q2 in zip(chain[:-1], chain[1:]))
-        log_error += sum(np.log(prop.readout_error(q)) for q in chain)
+        log_gate_error = sum(np.log(prop.gate_error('cx', [q1, q2])) for q1, q2 in zip(chain[:-1], chain[1:]))
+        log_readout_error = sum(np.log(prop.readout_error(q)) for q in chain)
         
-        if log_error < min_log_error:
-            min_log_error = log_error
+        if log_gate_error + log_readout_error < min_log_gate_error + min_log_readout_error:
+            min_log_gate_error = log_gate_error
+            min_log_readout_error = log_readout_error
             best_chain = chain
-            
-    return best_chain
+
+    if return_error_prod:
+        return best_chain, min_log_gate_error, min_log_readout_error
+    else:
+        return best_chain
